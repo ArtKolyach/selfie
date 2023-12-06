d5495255cd2f7176b7cd9650f207866871424dd5
ArtKolyach <artyom.kolyach@yandex.ru>
2023-12-06 16:20:27 +0300

    Initial commit

---
+++ auth.js
@@ -0,0 +1,18 @@
+import {index} from "./modules";
+import {passwordCheck} from "./modules/passwordCheck.js";
+
+const authWrapper = document.getElementById("auth-wrapper")
+const passwordInput = document.getElementById("password-input")
+const contentWrapper = document.getElementsByClassName("content-wrapper")
+
+passwordInput.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+        if (passwordCheck(e.target.value)) {
+            index()
+            contentWrapper[0].classList.remove("hidden")
+            authWrapper.hidden = true
+        } else  e.target.value = ""
+    }
+})
+
+passwordInput.addEventListener("change", (e) => console.log(e))
---
+++ modules/index.js
@@ -0,0 +1,81 @@
+export function index ()
+{
+
+    const image = document.querySelector('img')
+    const input = document.getElementById('input')
+    const playBtn = document.querySelector("#play")
+    const stopBtn = document.querySelector("#stop")
+    const nextBtn = document.querySelector("#next")
+    const backBtn = document.querySelector('#back')
+    const frameNumber = document.querySelector('#frame-number')
+
+    image.setAttribute("src", "../img/img_split/frame_00_delay-0.05s.gif")
+
+    const changePhoto = () => {
+        let frame = 0
+
+        return function (direction) {
+            if (direction === "forward") {
+                if (frame >= 29) {
+                    frame = 0
+                    return frame
+                } else return (++frame)
+            }
+            if (direction === "back") {
+                if (frame <= 0) {
+                    frame = 29
+                    return frame
+                } else return (--frame)
+            }
+        }
+    }
+
+    const parseFrameNumber = (frame) => {
+        if (frame < 10) return `0${frame}`
+        return String(frame)
+    }
+
+    let newFrame = changePhoto()
+
+    let intervalId = setInterval(() => {
+    }, 100)
+
+    const setPhotoInterval = () => {
+        intervalId = setInterval(() => {
+            const frame = newFrame("forward")
+            frameNumber.textContent = frame
+            image.setAttribute("src", `../img/img_split/frame_${parseFrameNumber(frame)}_delay-0.05s.gif`)
+        }, 1000 - input.value)
+    }
+
+    input.addEventListener("change", (e) => {
+        clearInterval(intervalId)
+        setPhotoInterval()
+    })
+
+    image.addEventListener("mousedown", () => {
+        clearInterval(intervalId)
+    })
+    image.addEventListener("mouseup", setPhotoInterval)
+    image.addEventListener("touchstart", () => clearInterval(intervalId))
+    image.addEventListener("touchend", setPhotoInterval)
+
+    playBtn.addEventListener("click", () => {
+        if (intervalId) clearInterval(intervalId)
+        setPhotoInterval()
+    })
+    stopBtn.addEventListener('click', () => clearInterval(intervalId))
+
+    nextBtn.addEventListener("click", () => {
+        const frame = newFrame("forward")
+        image.setAttribute("src", `../img/img_split/frame_${parseFrameNumber(frame)}_delay-0.05s.gif`)
+        frameNumber.textContent = frame
+    })
+
+    backBtn.addEventListener("click", () => {
+        const frame = newFrame("back")
+        image.setAttribute("src", `../img/img_split/frame_${parseFrameNumber(frame)}_delay-0.05s.gif`)
+        frameNumber.textContent = frame
+    })
+}
+
